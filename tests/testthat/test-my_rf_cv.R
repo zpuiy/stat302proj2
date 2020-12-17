bb821bcbd30f0f2cfbda32b0053f71d5af6488b3
Peiyao Zhu <pyzhu7@uw.edu>
2020-12-17 09:01:37 +0800

    Update tests

---
+++ tests/testthat/test-my_rf_cv.R
@@ -0,0 +1,3 @@
+test_that("my_rf_cv works", {
+  expect_is(my_rf_cv(5), "numeric")
+})
--- tests/testthat/test-my_t_test.R
+++ tests/testthat/test-my_t_test.R
@@ -1,29 +1,30 @@
 test_that("my_t_test works when alternatife is two.sided", {
-  expect_is(my_t_test(my_gapminder$lifeExp, "two.sided", 60), "list")
-  expect_is(my_t_test(my_gapminder$lifeExp, "two.sided", 60)$test_stat,
+  set.seed(100)
+  expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "two.sided", 0), "list")
+  expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "two.sided", 0)$test_stat,
             "numeric")
-  expect_is(my_t_test(my_gapminder$lifeExp, "two.sided", 60)$df, "numeric")
-  expect_is(my_t_test(my_gapminder$lifeExp, "two.sided", 60)$alternative,
+  expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "two.sided", 0)$df, "numeric")
+  expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "two.sided", 0)$alternative,
             "character")
-  expect_is(my_t_test(my_gapminder$lifeExp, "two.sided", 60)$p_val, "numeric")
+  expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "two.sided", 0)$p_val, "numeric")
 })
 
 test_that("my_t_test works when alternatife is less", {
-  expect_is(my_t_test(my_gapminder$lifeExp, "less", 60), "list")
-  expect_is(my_t_test(my_gapminder$lifeExp, "less", 60)$test_stat,
+  expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "less", 0), "list")
+  expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "less", 0)$test_stat,
             "numeric")
-  expect_is(my_t_test(my_gapminder$lifeExp, "less", 60)$df, "numeric")
-  expect_is(my_t_test(my_gapminder$lifeExp, "less", 60)$alternative,
+  expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "less", 0)$df, "numeric")
+  expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "less", 0)$alternative,
             "character")
-  expect_is(my_t_test(my_gapminder$lifeExp, "less", 60)$p_val, "numeric")
+  expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "less", 0)$p_val, "numeric")
 })
 
 test_that("my_t_test works when alternatife is greater", {
-  expect_is(my_t_test(my_gapminder$lifeExp, "greater", 60), "list")
-  expect_is(my_t_test(my_gapminder$lifeExp, "greater", 60)$test_stat,
+  expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "greater", 0), "list")
+  expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "greater", 0)$test_stat,
             "numeric")
-  expect_is(my_t_test(my_gapminder$lifeExp, "greater", 60)$df, "numeric")
-  expect_is(my_t_test(my_gapminder$lifeExp, "greater", 60)$alternative,
+  expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "greater", 0)$df, "numeric")
+  expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "greater", 0)$alternative,
             "character")
-  expect_is(my_t_test(my_gapminder$lifeExp, "greater", 60)$p_val, "numeric")
+  expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "greater", 0)$p_val, "numeric")
 })
