9c2e52a5ad602f174c162b164c26e5cf6cf0f640
Peiyao Zhu <pyzhu7@uw.edu>
2020-12-17 09:12:24 +0800

    update my_t_test test

--- tests/testthat/test-my_t_test.R
+++ tests/testthat/test-my_t_test.R
@@ -1,5 +1,4 @@
 test_that("my_t_test works when alternatife is two.sided", {
-  set.seed(100)
   expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "two.sided", 0), "list")
   expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "two.sided", 0)$test_stat,
             "numeric")
@@ -27,4 +26,9 @@ test_that("my_t_test works when alternatife is greater", {
   expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "greater", 0)$alternative,
             "character")
   expect_is(my_t_test(rnorm(100, mean = 0, sd = 1), "greater", 0)$p_val, "numeric")
+})
+
+test_that("unaccepted alternative input shows message", {
+  expect_error(my_t_test(rnorm(100, mean = 0, sd = 1), 1, 0)$alternative,
+                 "Alternative value must be 'two.sided', 'less', or 'greater'.")
 })
